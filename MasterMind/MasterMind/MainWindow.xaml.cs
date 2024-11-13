a6f533dd75da9e7b9a662e5810b497a1f4517b82
LorenzoLatinnePXL <11701160@student.pxl.be>
2024-11-13 10:12:37 +0100

    MasterMind-02

--- MasterMind/MasterMind/MainWindow.xaml.cs
+++ MasterMind/MasterMind/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,66 @@ namespace MasterMind
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Variables
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random();
+        string color1, color2, color3, color4;
+        string[] solution;
+        string[] options = { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Set current Title in the StringBuilder.
+            sb.Append(this.Title);
+
+            // Randomize colors.
+            color1 = GenerateRandomColor();
+            color2 = GenerateRandomColor();
+            color3 = GenerateRandomColor();
+            color4 = GenerateRandomColor();
+            solution = new string[] { color1, color2, color3, color4 };
+
+            // Set randomized colors in the StringBuilder.
+            sb.Append($" {color1}, {color2}, {color3}, {color4}");
+
+            // Change Title to data from the StringBuilder.
+            this.Title = sb.ToString();
+
+        }
+
+        private string GenerateRandomColor()
+        {
+            int randomColor = rnd.Next(0, 6);
+
+            switch (randomColor)
+            {
+                case 0:
+                    return options[0];
+
+                case 1:
+                    return options[1];
+
+                case 2:
+                    return options[2];
+
+                case 3:
+                    return options[3];
+
+                case 4:
+                    return options[4];
+
+                case 5:
+                    return options[5];
+
+                default:
+                    return "Color choice out of range.";
+            }
         }
     }
 }
